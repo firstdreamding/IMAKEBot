62cf28c4dec421c6fb1838766ca6fe354fa55697
firstdreamding <firstdreamding@gmail.com>
2022-02-17 20:08:39 -0600

    INIT

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.message.MessageFlag;
+import org.javacord.api.interaction.SlashCommandInteraction;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // Insert your bot's token here
+        String token = new String(Files.readAllBytes( Paths.get("src/main/resources/token_key.txt")));
+
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        // Add a listener which answers with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
+
+        // Print the invite url of your bot
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+    }
+}
