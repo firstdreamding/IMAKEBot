368b95fae627f4d7850f33cf26f6f190f6a23a71
Sparshdeep Singh <54998926+ssingh85@users.noreply.github.com>
2022-02-20 01:27:45 -0600

    parse tournament stats

--- src/main/java/BattlefyScraper.java
+++ src/main/java/BattlefyScraper.java
@@ -9,16 +9,17 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class BattlefyScraper {
 
     private static JSONArray tournamentArray;
+    private static JSONArray tournamentStatsArray;
     private final ArrayList<String> teamsArray;
 
     public BattlefyScraper(String battlefyUrl) throws ParseException, IOException {
         String[] splitBattlefyUrl = battlefyUrl.split("/");
         String tournamentId = splitBattlefyUrl[5];
-
         URL url = new URL("https://dtmwra1jsgyb0.cloudfront.net/tournaments/" + tournamentId + "/teams?");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
@@ -37,14 +38,54 @@ public class BattlefyScraper {
         tournamentArray = (JSONArray) parser.parse(content.toString());
         teamsArray = getTeams();
 
+        URL statsUrl = new URL("https://dtmwra1jsgyb0.cloudfront.net/stages/61d8c2ccb3ccd7231aa77fa0/stats");
+        HttpURLConnection con2 = (HttpURLConnection) statsUrl.openConnection();
+        con2.setRequestMethod("GET");
+
+        int status2 = con2.getResponseCode();
+        BufferedReader in2 = new BufferedReader(new InputStreamReader(con2.getInputStream()));
+        String inputLine2;
+        StringBuilder content2 = new StringBuilder();
+        while ((inputLine2 = in2.readLine()) != null) {
+            content2.append(inputLine2);
+        }
+        in2.close();
+        con2.disconnect();
+        JSONParser parser2 = new JSONParser();
+        tournamentStatsArray = (JSONArray) parser2.parse(content2.toString());
+
     }
 
     public static void main(String[] args) throws IOException, ParseException {
         // Insert your bot's token here
-        BattlefyScraper battlefyScraper = new
-                BattlefyScraper("https://battlefy.com/college-league-of-legends/2022-north-conference/6171f253947ed60d0abb9083/info?infoTab=details");
-        battlefyScraper.getTeams();
-        //System.out.println(tournamentArray);
+        //BattlefyScraper battlefyScraper = new
+            //    BattlefyScraper("https://battlefy.com/college-league-of-legends/2022-north-conference/6171f253947ed60d0abb9083/info?infoTab=details");
+        //battlefyScraper.getTeams();
+        BattlefyScraper battlefyScraper = new BattlefyScraper("https://battlefy.com/college-league-of-legends/2022-east-conference/61720cd843d5751156d83bd2/info?infoTab=details");
+
+        //System.out.println(tournamentStatsArray);
+        getKdas();
+    }
+
+    public static void getKdas() {
+        ArrayList<Object> statsArray = new ArrayList<>();
+        for (Object o : tournamentStatsArray) {
+            statsArray.add(((JSONObject) o).get("stats"));
+        }
+        System.out.println(statsArray.get(0));
+
+        ArrayList<Object> teamssArray = new ArrayList<>();
+        for (Object o : statsArray) {
+            teamssArray.add(((JSONObject) o).get("teams"));
+        }
+        System.out.println(teamssArray.get(0));
+
+        ArrayList<JSONArray> playersArray = new ArrayList<>();
+        for (Object o : teamssArray) {
+            playersArray.add((JSONArray) (((JSONObject) (((JSONArray) o).get(0))).get("players")));
+            playersArray.add((JSONArray) (((JSONObject) (((JSONArray) o).get(1))).get("players")));
+        }
+        System.out.println(playersArray);
     }
 
     public ArrayList<String> getTeams() {
