418bece948fa1d6a363c450134f3e31cbf654298
firstdreamding <firstdreamding@gmail.com>
2022-02-20 03:40:52 -0600

    Finished Almost with Help

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
@@ -20,7 +21,9 @@ public class Main {
     private DiscordApi api;
     private Twitch twitch;
 
-    private static BattlefyScraper battlefyScraper;
+    static BattlefyScraper battlefyScraper;
+
+    private HashMap<String, PageHandler> instances_teams;
 
     public static void main(String[] args) throws IOException, ParseException {
         // Insert your bot's token here
@@ -28,6 +31,7 @@ public class Main {
 
         String token = new String(Files.readAllBytes( Paths.get("src/main/resources/token_key.txt")));
         Main main = new Main();
+        main.instances_teams = new HashMap<>();
         main.api = new DiscordApiBuilder().setToken(token).login().join();
         main.twitterScraper = new TwitterScraper();
 
@@ -57,8 +61,29 @@ public class Main {
         }
 
         String messageLower = event.getMessageContent().toLowerCase();
+        if (messageLower.startsWith("jc help")) {
+            EmbedBuilder embed = new EmbedBuilder()
+                    .setAuthor("JC Help", "", "https://www.iconsdb.com/icons/preview/red/question-mark-xxl.png")
+                    .setTitle("JC Help")
+                    .setDescription("**OverallInfo**\n" +
+                            "jc search <University Name or Team Name>\n" +
+                            "    - Gets information on search query esports organization.\n" +
+                            "\n" +
+                            "jc news <University Name or Team Name>\n" +
+                            "    - Gets info if there on Universities Announcementt channel. \n" +
+                            "\n" +
+                            "**Twitter**\n" +
+                            "jc trecent <University Name or Team Name>\n" +
+                            "    - Get the most recent tweet from query organization (last week)  \n" +
+                            "\n" +
+                            "jc twitter <University Name or Team Name>\n" +
+                            "    - Get the Twitter account of the university and the JustChilln Score (scored via last week's likes, retweets, and replies)\n" +
+                            "\n" +
+                            "jc tstats <University Name or Team Name>\n" +
+                            "    - Get the Twitter rankings of all the database Universities Twitter account (last week)");
 
-        if (messageLower.startsWith("jc search")) {
+            event.getChannel().sendMessage(embed);
+        }else if (messageLower.startsWith("jc search")) {
             String[] output = messageLower.split("jc search", 2);
 
             if (output[1].trim().length() == 0) {
@@ -131,8 +156,8 @@ public class Main {
                     });
                 }
             }
-        } else if (messageLower.startsWith("jc twitter")) {
-            String[] output = messageLower.split("jc twitter", 2);
+        } else if (messageLower.startsWith("jc trecent")) {
+            String[] output = messageLower.split("jc trecent", 2);
             if (collegeNames.nameLookup.containsKey(output[1].trim())) {
                 int index = collegeNames.nameLookup.get(output[1].trim());
                 CollegeData currentData = collegeNames.collegeData.get(index);
@@ -146,22 +171,30 @@ public class Main {
             }
         } else if (messageLower.startsWith("jc tstats")) {
             twitterScraper.getAllStats(event.getChannel(), collegeNames.collegeData);
+        } else if (messageLower.startsWith("jc twitter")) {
+            String[] output = messageLower.split("jc twitter", 2);
+            if (collegeNames.nameLookup.containsKey(output[1].trim())) {
+                int index = collegeNames.nameLookup.get(output[1].trim());
+                CollegeData currentData = collegeNames.collegeData.get(index);
+
+                EmbedBuilder embed = new EmbedBuilder()
+                        .setAuthor(currentData.getName(), currentData.getTwitter(), currentData.getIcon())
+                        .setTitle(currentData.getName() + " Twitter Stats")
+                        .setThumbnail(currentData.getIcon())
+                        .setDescription("Last week points: " + twitterScraper.getLikes(currentData.getTwitter().split("https://twitter.com/", 2)[1], currentData).score);
+                event.getChannel().sendMessage(embed);
+            }
         } else if (messageLower.startsWith("jc teams")) {
             ArrayList<String> teamData = battlefyScraper.getTeams();
+
             teamData.sort(String::compareToIgnoreCase);
-            EmbedBuilder embed = new EmbedBuilder().setTitle("Teams").setThumbnail("https://cdn.battlefy.com/helix/images/leagues-v2/collegelol/clol-logo.png");
 
-            ArrayList<Button> buttons = new ArrayList<>();
-            buttons.add(new Button("<"));
-            buttons.add(new Button(">"));
-
-            StringBuilder description = new StringBuilder();
-            for (int i = 0; i < teamData.size(); i++) {
-                description.append("#").append(i + 1).append(" - ").append(teamData.get(i)).append("\n");
-
+            if (instances_teams.containsKey(event.getChannel().getIdAsString())) {
+                instances_teams.remove(event.getChannel().getIdAsString());
             }
-            embed.setDescription(description.toString());
-            event.getChannel().sendMessage(embed);
+
+            instances_teams.put(event.getChannel().getIdAsString(), new PageHandler(event.getChannel(), teamData, "Battlefly Teams", "Sparsh", "https://www.linkedin.com/in/sparshdeep-singh-08a07b221", "https://media-exp1.licdn.com/dms/image/C5603AQH7TMwhExoPjA/profile-displayphoto-shrink_800_800/0/1631757790166?e=1651104000&v=beta&t=d-G8wpbVq4DexYtHJz_WSvWnIXk0Tpi2LYkARHRIEoI", null));
+
         } else if (messageLower.startsWith("jc op")) {
             String teamName = messageLower.substring(6).trim();
             //System.out.println(teamName);
