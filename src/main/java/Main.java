b021f55afa89687c2664b1fb07b865a5e2f25307
Sparshdeep Singh <54998926+ssingh85@users.noreply.github.com>
2022-02-19 23:01:50 -0600

    implemented battlefy functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.emoji.Emoji;
 import org.javacord.api.entity.message.MessageFlag;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.interaction.SlashCommandInteraction;
+import org.json.simple.parser.ParseException;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -14,17 +16,19 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
-public class Main extends BattlefyScraper {
+public class Main {
 
     private CollegeNames collegeNames;
     private DiscordApi api;
+    private static BattlefyScraper battlefyScraper;
 
-    public static void main(String[] args) throws IOException {
-//        // Insert your bot's token here
-//        BattlefyScraper battlefyScraper = new BattlefyScraper();
-//        battlefyScraper.getPlayers("https://battlefy.com/college-league-of-legends/2022-north-conference/6171f253947ed60d0abb9083/info?infoTab=details");
+    public static void main(String[] args) throws IOException, ParseException {
+        // Insert your bot's token here
+        battlefyScraper = new BattlefyScraper("https://battlefy.com/college-league-of-legends/2022-north-conference/6171f253947ed60d0abb9083/info?infoTab=details");
+
         String token = new String(Files.readAllBytes( Paths.get("src/main/resources/token_key.txt")));
         Main main = new Main();
         main.api = new DiscordApiBuilder().setToken(token).login().join();
@@ -33,14 +37,20 @@ public class Main extends BattlefyScraper {
 
         // Add a listener which answers with "Pong!" if someone writes "!ping"
         main.api.addMessageCreateListener(event -> {
-            main.handleCommand(event);
+            try {
+                main.handleCommand(event);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
         });
 
         // Print the invite url of your bot
         System.out.println("You can invite the bot by using the following url: " + main.api.createBotInvite());
     }
 
-    public void handleCommand(MessageCreateEvent event) {
+    public void handleCommand(MessageCreateEvent event) throws IOException, ParseException {
         //If not an actual user (ie bot wrote the message)
         if (!event.getMessageAuthor().isRegularUser()) {
             return;
@@ -84,6 +94,29 @@ public class Main extends BattlefyScraper {
                     e.printStackTrace();
                 }
             }
+        } else if (messageLower.startsWith("jc teams")) {
+            ArrayList<String> teamData = battlefyScraper.getTeams();
+            teamData.sort(String::compareToIgnoreCase);
+            EmbedBuilder embed = new EmbedBuilder().setTitle("Teams").setThumbnail("https://cdn.battlefy.com/helix/images/leagues-v2/collegelol/clol-logo.png");
+
+            ArrayList<Button> buttons = new ArrayList<>();
+            buttons.add(new Button("<"));
+            buttons.add(new Button(">"));
+
+            StringBuilder description = new StringBuilder();
+            for (int i = 0; i < teamData.size(); i++) {
+                description.append("#").append(i + 1).append(" - ").append(teamData.get(i)).append("\n");
+
+            }
+            embed.setDescription(description.toString());
+            event.getChannel().sendMessage(embed);
+        } else if (messageLower.startsWith("jc op")) {
+            String teamName = messageLower.substring(6).trim();
+            //System.out.println(teamName);
+            String teamOpgg = battlefyScraper.getPlayersOpgg(teamName);
+            EmbedBuilder embed = new EmbedBuilder().setDescription(teamOpgg);
+            event.getChannel().sendMessage(embed);
         }
     }
+
 }
